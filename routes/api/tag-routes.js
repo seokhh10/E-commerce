40a0c44ea2b1fb93e1ef09ac4b5aad59e6199199
seokhh10 <ernesto.hidalgo.hdz@gmail.com>
2024-07-15 19:14:46 -0500

    working on tag-routes.js, router.PUT update a tag's name by id, done

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -52,6 +52,22 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(tagData => {
+    if (!tagData) {
+      res.status(404).json({ message: 'Tag ID not found'});
+      return;
+    }
+    res.json(tagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
